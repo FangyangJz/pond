bc16878bbfc0f0331f968268ad4f70a05fc19b69
fangyang_gpu <fangyang.jing@hotmail.com>
2023-11-08 01:17:27 +0800

    Add stock info table

--- pond/duckdb/__init__.py
+++ pond/duckdb/__init__.py
@@ -12,11 +12,13 @@ class DuckDB:
         self.path = db_path
 
         self.path_stock = db_path / 'stock'
+        self.path_stock_info = self.path_stock / 'info'
         self.path_stock_kline_1d = self.path_stock / 'kline_1d'
         self.path_stock_trades = self.path_stock / 'trades'
         self.path_stock_trades_origin = self.path_stock_trades / 'origin'
         self.path_stock_list = [
-            self.path_stock, self.path_stock_kline_1d, self.path_stock_trades, self.path_stock_trades_origin
+            self.path_stock, self.path_stock_info, self.path_stock_kline_1d,
+            self.path_stock_trades, self.path_stock_trades_origin
         ]
 
         self.path_bond = db_path / 'bond'
@@ -30,24 +32,36 @@ class DuckDB:
     def init_db_path(self):
         [f.mkdir() for f in self.path_list if not f.exists()]
 
-    def update_bond_basic(self):
+    def update_bond_info(self):
         from pond.akshare.bond import get_bond_basic_df
 
+        start_time = time.perf_counter()
         bond_basic_df, bond_redeem_df = get_bond_basic_df()
         bond_basic_df['上市时间'] = pd.to_datetime(bond_basic_df['上市时间'])
         bond_basic_df['转股起始日'] = pd.to_datetime(bond_basic_df['转股起始日'])
         bond_redeem_df['转股起始日'] = pd.to_datetime(bond_redeem_df['转股起始日'])
 
         compress = 'ZSTD'
-        start_time = time.perf_counter()
+
         (self.con.sql('select * from bond_basic_df')
             .write_parquet(str(self.path_bond_info / f'basic.parquet'), compression=compress))
         logger.success(f'Update basic.parquet cost: {time.perf_counter() - start_time}s')
 
-        start_time = time.perf_counter()
         (self.con.sql('select * from bond_redeem_df')
          .write_parquet(str(self.path_bond_info / f'redeem.parquet'), compression=compress))
         logger.success(f'Update redeem.parquet cost: {time.perf_counter() - start_time}s')
+
+    def update_stock_info(self):
+        from pond.akshare.stock import get_all_stocks_df
+
+        start_time = time.perf_counter()
+        stock_basic_df = get_all_stocks_df()
+
+        compress = 'ZSTD'
+
+        (self.con.sql('select * from stock_basic_df')
+            .write_parquet(str(self.path_stock_info / f'basic.parquet'), compression=compress))
+        logger.success(f'Update basic.parquet cost: {time.perf_counter() - start_time}s')
 
     def update_stock_trades(self):
         from pond.stock.trades import get_trade_df_with_multiprocess
@@ -66,11 +80,13 @@ class DuckDB:
 
 if __name__ == '__main__':
     db = DuckDB(Path(r'D:\DuckDB'))
-    db.update_bond_basic()
+    db.update_stock_info()
+    db.update_bond_info()
     # db.update_stock_trades()
     r1 = db.con.sql(
         rf"SELECT * from read_parquet('{str(db.path_stock_trades/'20230504.parquet')}')")  # order by jj_code, datetime
 
     r2 = db.con.sql(rf"SELECT * from read_parquet('{str(db.path_bond_info / 'basic.parquet')}')")
     r3 = db.con.sql(rf"SELECT * from read_parquet('{str(db.path_bond_info / 'redeem.parquet')}')")
+    r4 = db.con.sql(rf"SELECT * from read_parquet('{str(db.path_stock_info / 'basic.parquet')}')")
     print(1)
