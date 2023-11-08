a84e214a5c0b581e5c81a5ba75fa15036c0a624c
fangyang_gpu2 <fangyang.jing@hotmail.com>
2023-11-09 01:26:49 +0800

    [Add] aggTrade data

--- pond/duckdb/crypto/__init__.py
+++ pond/duckdb/crypto/__init__.py
@@ -22,10 +22,14 @@ class CryptoDB(DuckDB):
         self.path_crypto_kline_1d = self.path_crypto / 'kline_1d'
         self.path_crypto_trades = self.path_crypto / 'trades'
         self.path_crypto_trades_origin = self.path_crypto_trades / 'origin'
+        self.path_crypto_agg_trades = self.path_crypto / 'agg_trades'
+        self.path_crypto_agg_trades_origin = self.path_crypto_agg_trades / 'origin'
+
         self.path_crypto_list = [
             self.path_crypto, self.path_crypto_info,
             self.path_crypto_kline_1d,
-            self.path_crypto_trades, self.path_crypto_trades_origin
+            self.path_crypto_trades, self.path_crypto_trades_origin,
+            self.path_crypto_agg_trades, self.path_crypto_agg_trades_origin,
         ]
 
         super().__init__(db_path)
@@ -34,14 +38,41 @@ class CryptoDB(DuckDB):
         [f.mkdir() for f in self.path_crypto_list if not f.exists()]
 
     def update_crypto_trades(self):
+
+        trades_list = [f.stem for f in self.path_crypto_trades.iterdir()]
+
         for f in (pbar := tqdm(self.path_crypto_trades_origin.glob('*.csv'))):
             pbar.set_postfix_str(str(f))
-            (self.sql(
-                f"SELECT id, price, qty, quote_qty, epoch_ms(time) as time, is_buyer_maker  "
-                f"from read_csv_auto('{str(f)}') order by id")
-             .write_parquet(str(self.path_crypto_trades / f'{f.stem}.parquet'), compression=self.compress))
+            if f.stem not in trades_list:
+                (self.con.sql(
+                    f"SELECT id, price, qty, quote_qty, epoch_ms(time) as time, is_buyer_maker  "
+                    f"from read_csv_auto('{str(f)}') order by id")
+                 .write_parquet(str(self.path_crypto_trades / f'{f.stem}.parquet'), compression=self.compress))
+
+    def update_crypto_agg_trades(self):
+
+        agg_trades_list = [f.stem for f in self.path_crypto_agg_trades.iterdir()]
+        names = [
+            'agg_trade_id', 'price', 'qty', 'first_trade_id',
+            'last_trade_id', 'transact_time', 'is_buyer_maker'
+        ]
+        for f in (pbar := tqdm(self.path_crypto_agg_trades_origin.glob('*.csv'))):
+            pbar.set_postfix_str(str(f))
+            if f.stem not in agg_trades_list:
+                (self.con.sql(
+                    f"SELECT agg_trade_id, price, qty, first_trade_id, last_trade_id, "
+                    f"epoch_ms(transact_time) as transact_time, is_buyer_maker  "
+                    f"from read_csv_auto('{str(f)}', names={names})")
+                 .write_parquet(str(self.path_crypto_agg_trades / f'{f.stem}.parquet'), compression=self.compress))
 
 
 if __name__ == '__main__':
-    db = CryptoDB(Path(r'D:\DuckDB'))
+    # db = CryptoDB(Path(r'D:\DuckDB'))
+    db = CryptoDB(Path(r'/home/fangyang/zhitai5000/DuckDB/'))
     # db.update_crypto_trades()
+    # for f in (pbar := tqdm(db.path_crypto_agg_trades_origin.glob('*.csv'))):
+    #     r = db.con.read_csv(
+    #         f,
+    #         names=['agg_trade_id', 'price', 'qty', 'first_trade_id', 'last_trade_id', 'transact_time', 'is_buyer_maker'])
+    #     print(1)
+    db.update_crypto_agg_trades()
