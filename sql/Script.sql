f0bc9ad3a00295a34fed2daf1721d8bf87f7837f
fangyang_gpu <fangyang.jing@hotmail.com>
2023-11-10 03:29:24 +0800

    [Add] sql cheat sheet

--- sql/Script.sql
+++ sql/Script.sql
@@ -1,1 +1,5 @@
-SELECT * from read_parquet('D:\DuckDB\stock\trades\20230504.parquet');
+create table t1 as from read_parquet('D:\DuckDB\stock\trades\20230504.parquet');
+
+SELECT * from t1 limit 10;
+SELECT * from t1 where extract('hour' from datetime) <= 9 and extract('minute' from datetime) <= 20 ;
+SELECT * from t1 where datetime::TIME <= TIME '09:20:00';
