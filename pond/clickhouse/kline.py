8636b96dec623c7a910fe75f9f0b3b91772e4e60
heavy <heavy@163.com>
2024-01-27 13:35:39 +0800

    [Add] kline downloading into clickhouse with akshare

---
+++ pond/clickhouse/kline.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Column, func
+from clickhouse_sqlalchemy import (
+    types, engines
+)
+from pond.clickhouse import TsTable
+
+import akshare as ak
+
+def stock_zh_a_hist(**kwargs):
+    print(f"stock_zh_a_hist {kwargs}")
+    df = ak.stock_zh_a_hist(**kwargs)
+    if df is not None:
+        df["代码"] = kwargs['symbol']
+        print(f"stock_zh_a_hist {kwargs['symbol']}, size {len(df)}")
+    return df
+
+class KlineDailyHFQ(TsTable):
+    """
+    日K线，后复权
+    """
+
+    __tablename__ = "kline_daily_hfq"
+
+    datetime = Column(types.DateTime64, comment="日期", primary_key=True)
+    code = Column(types.String, comment="代码")
+    open = Column(types.Float64, comment="开盘")
+    high = Column(types.Float64, comment="最高")
+    low = Column(types.Float64, comment="最低")
+    close = Column(types.Float64, comment="收盘")
+    volume = Column(types.Float64, comment="成交量")
+    amount = Column(types.Float64, comment="成交额")
+    turn = Column(types.Float64, comment="换手率")
+    
+    __table_args__ = (
+        engines.MergeTree(
+            partition_by=func.toYYYYMM(datetime),
+            order_by=(datetime, code),
+            primary_key=(datetime, code)
+        ),
+    )
--- pond/clickhouse/manager.py
+++ pond/clickhouse/manager.py
@@ -7,8 +7,9 @@ import pandas as pd
 from pond.clickhouse import metadata, TsTable
 from pond.clickhouse.holders import *
 from typing import List
-
-from sqlalchemy import create_engine, Column, MetaData, literal, text, func, desc
+from pond.akshare.stock import get_all_stocks_df
+from pond.clickhouse.kline import KlineDailyHFQ, stock_zh_a_hist
+from sqlalchemy import create_engine, Column, literal, text, func, desc
 
 from clickhouse_sqlalchemy import (
     make_session, get_declarative_base, types, engines
@@ -21,29 +22,41 @@ class Task:
         self.func = func
         self.arg_groups = arg_groups
         self.downloaders = []
-
+        self.distributed = True
+        self.dfs = []
 
 class ClickHouseManager:
 
-    def __init__(self, db_uri) -> None:
+    def __init__(self, db_uri, data_start:datetime) -> None:
         self.engine = create_engine(db_uri)
         metadata.create_all(self.engine)
         self.session = make_session(self.engine)
+        self.data_start = data_start
 
     def sync(self, date=datetime.now()):
         print(f"click house manager syncing at {date.isoformat()}")
         tasks = self.get_syncing_tasks(date)
+        max_dowloader_size = int(os.cpu_count() / len(tasks)) + 1
         for task in tasks:
             record = self.session.query(task.table).order_by(desc(task.table.datetime)).limit(1).one_or_none()
             if record is not None and record.datetime >= date:
                 continue
-            for kwargs in task.arg_groups:
-                downloader = Downloader.remote()
-                downloader.download.remote(task.func, **kwargs)
-                task.downloaders.append(downloader)
+            for i in range(len(task.arg_groups)):
+                kwargs = task.arg_groups[i]
+                if task.distributed:
+                    if len(task.downloaders) < max_dowloader_size:
+                        downloader = Downloader.remote()
+                        task.downloaders.append(downloader)
+                    else:
+                        downloader = task.downloaders[i % max_dowloader_size]
+                    downloader.download.remote(task.func, **kwargs)
+                else:
+                    df = task.func(**kwargs)
+                    if df is not None and len(df) > 0:
+                        task.dfs.append(df)
 
         for task in tasks:
-            dfs = []
+            dfs = task.dfs
             for downloader in task.downloaders:
                 df = ray.get(downloader.get.remote())
                 if df is not None and len(df) > 0:
@@ -70,6 +83,15 @@ class ClickHouseManager:
         holder_types = ["个人", "基金", "QFII", "社保", "券商", "信托"]
         changements = ["新进", "增加", "不变", "减少"]
         tasks.append(Task(FreeHoldingDetail, ak.stock_gdfx_free_holding_detail_em, [args]))
+
+        #kline daily hfq
+        stock_basic = get_all_stocks_df()
+        begin = self.get_cache_date(KlineDailyHFQ)
+        kline_daily_args = []
+        for symbol in stock_basic['代码']:
+            kline_daily_args.append({"symbol":symbol, 'start_date':datestr(begin), 'end_date':datestr(date), 'period':'daily', 'adjust':'hfq'})
+        tasks.append(Task(KlineDailyHFQ, stock_zh_a_hist, kline_daily_args))
+
         #holding detail
         holding_detail_arg_groups = []
         for holder_type in holder_types:
@@ -87,28 +109,32 @@ class ClickHouseManager:
         tasks.append(Task(HolderCounts, ak.stock_zh_a_gdhs, [{"symbol":datestr(date)}]))
 
         #restricted release detail
-        record = self.session.query(StockRestrictedReleaseDetail).order_by(desc(StockRestrictedReleaseDetail.datetime)).limit(1).one_or_none()
-        if record is not None:
-            begin = record.datetime
-        else:
-            begin = date - timedelta(days=365 * 2)
+        begin = self.get_cache_date(StockRestrictedReleaseDetail)
         restricted_release_detail_args = {"start_date":datestr(begin), "end_date":datestr(date)}
         tasks.append(Task(StockRestrictedReleaseDetail, ak.stock_restricted_release_detail_em, [restricted_release_detail_args]))
         return tasks
 
 
-
+    def get_cache_date(self, table:TsTable):
+        record = self.session.query(table).order_by(desc(table.datetime)).limit(1).one_or_none()
+        if record is not None:
+            begin = record.datetime
+        else:
+            begin = self.data_start
+        return begin
 
 if __name__ == "__main__":
     import os
     import time
     password = os.environ.get("CLICKHOUSE_PWD")
     conn_str = f"clickhouse://default:{password}@localhost:8123/quant"
-    manager = ClickHouseManager(conn_str)
-    begin = datetime(2019, 2, 2)
-    end = datetime.now()
-    while begin < end:
-        begin += timedelta(days=1)
-        manager.sync(date=begin)
-        time.sleep(2)
+    manager = ClickHouseManager(conn_str, data_start=datetime(2018,1,1))
+    begin = datetime(2024, 1, 26)
+    manager.sync(date=begin)
+
+    # end = datetime.now()
+    # while begin < end:
+    #     begin += timedelta(days=1)
+    #     manager.sync(date=begin)
+    #     time.sleep(2)
     
