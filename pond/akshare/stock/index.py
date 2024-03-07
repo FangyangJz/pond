29ca848fe1a28ee0a00843512d97fc764af7b227
heavy <heavy@163.com>
2024-03-07 14:49:20 +0800

    [Update] index cons api from akshare

--- pond/akshare/stock/index.py
+++ pond/akshare/stock/index.py
@@ -15,13 +15,13 @@ def get_all_index(cache_path:Path=None, update=False):
     return df
     
 
-def get_index_detail(symbol:str, cache_path:Path=None):
+def get_index_cons(symbol:str, cache_path:Path=None):
     cache_file = None
     if cache_path is not None:
         cache_file = cache_path / "cons" / f"{symbol}.csv"
         if cache_file.exists():
             return pd.read_csv(cache_file)
-    df = ak.index_stock_cons(symbol)
+    df = ak.index_stock_cons_sina(symbol)
     if cache_file is not None:
         cache_file.parent.mkdir(exist_ok=True)
         df.to_csv(cache_file)
