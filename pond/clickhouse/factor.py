da6f6ac2ebd4c1c55a9965e8fc655ceaa02319b6
heavy <heavy@163.com>
2024-04-08 21:16:59 +0800

    [Update] factors table, remove window column, add interval column

--- pond/clickhouse/factor.py
+++ pond/clickhouse/factor.py
@@ -9,8 +9,47 @@ class StockFactor(TsTable):
     datetime = Column(types.DateTime64, comment="close_time", primary_key=True)
     code = Column(types.String, comment="jj_code")
     clazz = Column(types.String, comment="factor_class")
+    interval = Column(types.String, comment="interval")
     name = Column(types.String, comment="factor_name")
-    window = Column(types.String, comment="window")
+    value = Column(types.Float64, comment="value")
+
+    __table_args__ = (
+        engines.MergeTree(
+            partition_by=func.toYYYYMM(datetime),
+            order_by=(datetime, code),
+            primary_key=(datetime, code),
+        ),
+    )
+
+
+class CryptoFactor(TsTable):
+
+    __tablename__ = "crypto_factor"
+
+    datetime = Column(types.DateTime64, comment="close_time", primary_key=True)
+    code = Column(types.String, comment="pair")
+    clazz = Column(types.String, comment="factor_class")
+    interval = Column(types.String, comment="interval")
+    name = Column(types.String, comment="factor_name")
+    value = Column(types.Float64, comment="value")
+
+    __table_args__ = (
+        engines.MergeTree(
+            partition_by=func.toYYYYMM(datetime),
+            order_by=(datetime, code),
+            primary_key=(datetime, code),
+        ),
+    )
+
+class FuturesFactor(TsTable):
+
+    __tablename__ = "futures_factor"
+
+    datetime = Column(types.DateTime64, comment="close_time", primary_key=True)
+    code = Column(types.String, comment="pair")
+    clazz = Column(types.String, comment="factor_class")
+    interval = Column(types.String, comment="interval")
+    name = Column(types.String, comment="factor_name")
     value = Column(types.Float64, comment="value")
 
     __table_args__ = (
